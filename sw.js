8d74871e58e32dd3cb8dd75be83bf56db75cc9cc
lsslss <lsslss@gmail.com>
2026-03-27 10:49:30 +0800

    更新到 v6.8

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'km-v67';
+const CACHE_NAME = 'km-v68';
 self.addEventListener('install', e => self.skipWaiting());
 self.addEventListener('activate', e => e.waitUntil(clients.claim()));
 self.addEventListener('fetch', e => { e.respondWith(fetch(e.request)); });
