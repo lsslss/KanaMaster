bd15816be4eca3b9335f9a4f63961625d4f5c96e
lsslss <lsslss@gmail.com>
2026-03-27 12:18:35 +0800

    更新至穏定版 v7.5

--- sw-stable.js
+++ sw-stable.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'km-stable-v74';
+const CACHE_NAME = 'km-stable-v75';
 self.addEventListener('install', e => self.skipWaiting());
 self.addEventListener('activate', e => e.waitUntil(clients.claim()));
 self.addEventListener('fetch', e => { e.respondWith(fetch(e.request)); });
