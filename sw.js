6f5b636da8ff8cbe49b945d2a8478bc54176cad5
lsslss <lsslss@gmail.com>
2026-03-27 11:39:50 +0800

    更新至 v7.1

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'km-v70';
+const CACHE_NAME = 'km-v71';
 self.addEventListener('install', e => self.skipWaiting());
 self.addEventListener('activate', e => e.waitUntil(clients.claim()));
 self.addEventListener('fetch', e => { e.respondWith(fetch(e.request)); });
